2eb9a9a05e74fc6eca07d84f57214057f041dc36
TH5ND3R <kristianrisagerjensen@gmail.com>
2023-01-16 15:14:01 +0100

    please work v1

--- Assets/Scripts/Other/PickableController2D.cs
+++ Assets/Scripts/Other/PickableController2D.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickableController : MonoBehaviour
+public class PickableController2D : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-       if (collision.tag == "Player")
+        if (collision.tag == "Player")
         {
             Destroy(gameObject);
         }
